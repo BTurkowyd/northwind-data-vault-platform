22734caf51b7f7cd5e569c039473ff999073380d
Bartosz Turkowyd <vertax@gmail.com>
2025-03-16 12:57:33 +0100

    #8: creating a bucket for scripts and data

---
+++ terraform/s3_buckets.tf
@@ -0,0 +1,13 @@
+# s3 bucket for scripts, raw data and data vault
+resource "aws_s3_bucket" "bucket" {
+  bucket = "ecommerce-bucket-${var.stage}-${var.aws_account_id}-${lower(random_id.bucket_suffix.b64_url)}"
+  force_destroy = true
+
+  tags = {
+    Name = "ecommerce-bucket-${var.stage}"
+  }
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 16
+}
