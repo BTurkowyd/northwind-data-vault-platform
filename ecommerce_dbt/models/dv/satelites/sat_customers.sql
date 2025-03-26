105796926d7300ce0ea2886c9b2076d8d38aa32e
Bartosz Turkowyd <vertax@gmail.com>
2025-03-27 00:08:59 +0100

    #7: creating hubs, links and satellites.

--- ecommerce_dbt/models/dv/satelites/sat_customers.sql
+++ ecommerce_dbt/models/dv/satelites/sat_customers.sql
@@ -12,18 +12,25 @@ WITH source_data AS (
 ),
 hub_keys AS (
     SELECT customer_id, hub_customer_key FROM {{ ref('hub_customers') }}
+),
+prepared AS (
+    SELECT
+        sd.*,
+        hk.hub_customer_key,
+        {{ dbt_utils.generate_surrogate_key(['sd.name', 'sd.email']) }} AS hashdiff
+    FROM source_data sd
+    JOIN hub_keys hk ON sd.customer_id = hk.customer_id
 )
 
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['hk.hub_customer_key', 'sd.hashdiff']) }} AS sat_customer_key,
-    hk.hub_customer_key,
-    sd.name,
-    sd.email,
-    {{ dbt_utils.generate_surrogate_key(['sd.name', 'sd.email']) }} AS hashdiff,
+    {{ dbt_utils.generate_surrogate_key(['hub_customer_key', 'hashdiff']) }} AS sat_customer_key,
+    hub_customer_key,
+    name,
+    email,
+    hashdiff,
     CAST(CURRENT_TIMESTAMP AS timestamp(6) with time zone) AS load_ts
-FROM source_data sd
-JOIN hub_keys hk ON sd.customer_id = hk.customer_id
+FROM prepared
+
 {% if is_incremental() %}
-WHERE {{ dbt_utils.generate_surrogate_key(['sd.name', 'sd.email']) }}
-    NOT IN (SELECT hashdiff FROM {{ this }})
+WHERE hashdiff NOT IN (SELECT hashdiff FROM {{ this }})
 {% endif %}
