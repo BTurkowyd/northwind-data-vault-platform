e9d0301cb33b9e41eaef2e940e513c1a6560fa79
Bartosz Turkowyd <vertax@gmail.com>
2025-04-10 21:31:30 +0200

    #32: create schema and grants to it

--- terraform/snowflake/snowflake_database.tf
+++ terraform/snowflake/snowflake_database.tf
@@ -2,3 +2,9 @@ resource "snowflake_database" "my_db" {
   name    = "NORTHWIND_DB"
   comment = "Northwind database created with Terraform"
 }
+
+resource "snowflake_schema" "northwind_schema" {
+  name     = "NORTHWIND_SCHEMA"
+  database = snowflake_database.my_db.name
+  comment  = "Northwind schema created with Terraform"
+}
--- terraform/snowflake/snowflake_roles.tf
+++ terraform/snowflake/snowflake_roles.tf
@@ -1,15 +1,24 @@
+
 resource "snowflake_account_role" "northwind_owner" {
   name    = "NORTHWIND_OWNER"
   comment = "Role to manage NORTHWIND_DB"
 }
 
 resource "snowflake_grant_privileges_to_account_role" "grant_ownership_db" {
-  privileges        = ["ALL PRIVILEGES"]
   account_role_name = snowflake_account_role.northwind_owner.name
+  all_privileges    = true
   on_account_object {
     object_type = "DATABASE"
     object_name = snowflake_database.my_db.name
   }
+}
+
+resource "snowflake_grant_privileges_to_account_role" "example" {
+  account_role_name = snowflake_account_role.northwind_owner.name
+  on_schema {
+    schema_name = snowflake_schema.northwind_schema.fully_qualified_name
+  }
+  all_privileges = true
 }
 
 resource "snowflake_grant_account_role" "parent_role_grant" {
