fcba93f58b1e360cb64e1bf7e640133405de262e
Bartosz Turkowyd <vertax@gmail.com>
2025-04-13 00:01:08 +0200

    #36: adding the storage bucket

--- terraform/snowflake/snowflake_integrations.tf
+++ terraform/snowflake/snowflake_integrations.tf
@@ -3,6 +3,6 @@ resource "snowflake_storage_integration" "s3_integration" {
   storage_provider = "S3"
   enabled          = true
   storage_allowed_locations = [
-    "s3://your-bucket/path/"
+    "s3://ecommerce-bucket-dev-926728314305-q1c4tvebvzy7chgggfuyva/northwind_data_vault/northwind_data_vault/"
   ]
 }
