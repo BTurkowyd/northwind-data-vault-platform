0e09da2a43b3f219eb2cc59b4b7aa9c22e095fb3
Bartosz Turkowyd <vertax@gmail.com>
2025-05-03 12:48:08 +0200

    #59: correcting policies

--- terraform/snowflake/snowflake_tags_masks.tf
+++ terraform/snowflake/snowflake_tags_masks.tf
@@ -5,8 +5,8 @@ resource "snowflake_tag" "pii" {
   comment        = "PII tag"
   allowed_values = ["true", "false"]
   masking_policies = [
-    snowflake_masking_policy.redact_varchar.fully_qualified_name,
-    snowflake_masking_policy.redact_number.fully_qualified_name
+    snowflake_masking_policy.redact_varchar_pii.fully_qualified_name,
+    snowflake_masking_policy.redact_number_pii.fully_qualified_name
   ]
 }
 
@@ -17,13 +17,13 @@ resource "snowflake_tag" "sensitivity" {
   comment        = "Sensitivity tag"
   allowed_values = ["low", "high", "critical"]
   masking_policies = [
-    snowflake_masking_policy.redact_varchar.fully_qualified_name,
-    snowflake_masking_policy.redact_number.fully_qualified_name
+    snowflake_masking_policy.redact_varchar_sensitivity.fully_qualified_name,
+    snowflake_masking_policy.redact_number_sensitivity.fully_qualified_name
   ]
 }
 
-resource "snowflake_masking_policy" "redact_varchar" {
-  name     = "REDACT_VARCHAR"
+resource "snowflake_masking_policy" "redact_varchar_pii" {
+  name     = "REDACT_VARCHAR_PII"
   database = snowflake_database.my_db.name
   schema   = snowflake_schema.northwind_schema.name
 
@@ -35,15 +35,16 @@ resource "snowflake_masking_policy" "redact_varchar" {
   body = <<-SQL
     CASE
       WHEN CURRENT_ROLE() = 'ACCOUNTADMIN' THEN input_val
-      ELSE 'REDACTED'
+      WHEN SYSTEM$GET_TAG_ON_CURRENT_COLUMN('pii') = 'true' THEN 'REDACTED'
+      WHEN SYSTEM$GET_TAG_ON_CURRENT_COLUMN('pii') = 'false' THEN input_val
     END
   SQL
 
   return_data_type = "VARCHAR"
 }
 
-resource "snowflake_masking_policy" "redact_number" {
-  name     = "REDACT_NUMBER"
+resource "snowflake_masking_policy" "redact_number_pii" {
+  name     = "REDACT_NUMBER_PII"
   database = snowflake_database.my_db.name
   schema   = snowflake_schema.northwind_schema.name
 
@@ -55,7 +56,52 @@ resource "snowflake_masking_policy" "redact_number" {
   body = <<-SQL
     CASE
       WHEN CURRENT_ROLE() = 'ACCOUNTADMIN' THEN input_val
-      ELSE -1
+      WHEN SYSTEM$GET_TAG_ON_CURRENT_COLUMN('pii') = 'true' THEN -1
+      WHEN SYSTEM$GET_TAG_ON_CURRENT_COLUMN('pii') = 'false' THEN input_val
+    END
+  SQL
+
+  return_data_type = "NUMBER"
+}
+
+resource "snowflake_masking_policy" "redact_varchar_sensitivity" {
+  name     = "REDACT_VARCHAR_SENSITIVITY"
+  database = snowflake_database.my_db.name
+  schema   = snowflake_schema.northwind_schema.name
+
+  argument {
+    name = "input_val"
+    type = "VARCHAR"
+  }
+
+  body = <<-SQL
+    CASE
+      WHEN CURRENT_ROLE() = 'ACCOUNTADMIN' THEN input_val
+      WHEN SYSTEM$GET_TAG_ON_CURRENT_COLUMN('sensitivity') = 'critical' THEN 'CRITICAL_SENSITIVITY'
+      WHEN SYSTEM$GET_TAG_ON_CURRENT_COLUMN('sensitivity') = 'high' THEN 'HIGH SENSITIVITY'
+      WHEN SYSTEM$GET_TAG_ON_CURRENT_COLUMN('sensitivity') = 'low' THEN input_val
+    END
+  SQL
+
+  return_data_type = "VARCHAR"
+}
+
+resource "snowflake_masking_policy" "redact_number_sensitivity" {
+  name     = "REDACT_NUMBER_SENSITIVITY"
+  database = snowflake_database.my_db.name
+  schema   = snowflake_schema.northwind_schema.name
+
+  argument {
+    name = "input_val"
+    type = "NUMBER"
+  }
+
+  body = <<-SQL
+    CASE
+      WHEN CURRENT_ROLE() = 'ACCOUNTADMIN' THEN input_val
+      WHEN SYSTEM$GET_TAG_ON_CURRENT_COLUMN('sensitivity') = 'critical' THEN -1
+      WHEN SYSTEM$GET_TAG_ON_CURRENT_COLUMN('sensitivity') = 'high' THEN -2
+      WHEN SYSTEM$GET_TAG_ON_CURRENT_COLUMN('sensitivity') = 'low' THEN input_val
     END
   SQL
 
